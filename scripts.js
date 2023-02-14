cab5ebd93e05394a0a972a93316b2913d686aaed
Ethan Mullins <54146188+EDMullins@users.noreply.github.com>
2023-02-14 14:08:02 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,14 +1,18 @@
+var listNum = 1;
+
 function add() {
-  var div = document.createElement("div");
-  div.classList.add('list');
-  document.getElementById("content").innerHTML += "<div class='list id='list'><center><form><label form='listlabel'></label><br><input type='text' id='listlabel' placeholder='Title It!' name='listlabel'><br></form><div class='desc'><textarea placeholder='Describe It!' id='desc' name='desc'></textarea></div></center></div>";
-  
-  }
-  
-  function remove() {
-  const div= document.getElementById('list');
-  div.classList.add('list');
-  div.remove();
-  }
-  
-  // Space for Comments
+    document.getElementById("content").innerHTML += "<div class='list' id='" + listNum + "'><center><form><label form='listlabel'></label><br><input type='text' id='listlabel' placeholder='Title It!' name='listlabel'><br></form><div class='desc'><textarea placeholder='Describe It!' id='desc' name='desc'></textarea></div></center></div>";
+
+    console.log(listNum);
+    listNum += 1;
+}
+     
+function remove() {
+    const div = document.getElementById(listNum);
+
+    if (listNum <= 1) {} else {listNum -= 1;}
+    
+    console.log(listNum);
+}
+    
+    // Space for Comments
